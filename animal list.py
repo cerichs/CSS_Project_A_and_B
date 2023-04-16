54f1d9d4fccefe1817dae4597089cec5621b0c78
Cornelius E <cornelius.erichs@gmail.com>
2023-04-16 13:56:10 +0200

    combining scripts and changes

--- animal list.py
+++ animal list.py
@@ -1,31 +1,65 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+from get_links import links_on_page
+import networkx
+from tqdm import tqdm
+def names_from_table():
+    url = "https://en.wikipedia.org/wiki/List_of_animal_names"
+    
+    response = requests.get(url)
+    html_content = response.content
+    
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    # Find table on the page
+    table = soup.find_all('table',{"class":"wikitable"})[1]
+    
+    # Find all the rows in the table
+    t_rows = table.find_all('tr')
+    
+    ths = t_rows[0].find_all('th')
+    
+    header = [th.text.replace("\n","") for th in ths]
+    
+    rows = []
+    names = {}
+    for tr in t_rows[1:]:
+        tds = tr.find_all('td')
+        #row = [td.text.replace("\n","") for td in tds]
+        if tds:
+            links = tds[0].find_all('a')
+            if links:
+                row = [td.text.replace("\n","") for td in tds]
+                rows.append(row)
+                cleaned_row = re.sub(r'\(.*\)|Also see.*|\[\d+\]|See.*', '', row[0])
+                #cleaned_row = re.sub(r'\[\d+\]', '', row[0])
+                #names[cleaned_row] = 0
+                for link in links:
+                    link_href = link.get('href')
+                    if link_href.startswith('/wiki/'):
+                        #link_href = 'https://en.wikipedia.org' + link_href
+                        names[link_href] = cleaned_row
+                        break
+    import pandas as pd
+    df_animals = pd.DataFrame(rows, columns=header)
 
-url = "https://en.wikipedia.org/wiki/List_of_animal_names"
+    return names
 
-response = requests.get(url)
-html_content = response.content
-
-soup = BeautifulSoup(html_content, 'html.parser')
-
-# Find table on the page
-table = soup.find_all('table',{"class":"wikitable"})[1]
-
-# Find all the rows in the table
-t_rows = table.find_all('tr')
-
-ths = t_rows[0].find_all('th')
-
-header = [th.text.replace("\n","") for th in ths]
-
-rows = []
-for tr in t_rows[1:]:
-    tds = tr.find_all('td')
-    row = [td.text.replace("\n","") for td in tds]
-    rows.append(row)                    
-import pandas as pd
-df_animals = pd.DataFrame(rows, columns=header)
-
-
-# Print the list of animal names
-print(df_animals)
+if __name__ == "__main__":
+    edgelist_weights = {}
+    animal_name = "Elephant"
+    names = names_from_table()
+    for name in tqdm(names.keys()):
+        result = links_on_page(animal_name=name.replace("/wiki/", ""))
+    
+        for entry in result:
+            if entry in names:
+                pair = ("/wiki/"+name,entry)
+                pair_inverted = (entry,"/wiki/"+name)
+                if pair in edgelist_weights:
+                    edgelist_weights[pair] += 1
+                elif pair_inverted in edgelist_weights:
+                    edgelist_weights[pair_inverted] = 1
+                else:
+                    edgelist_weights[pair] = 1
--- get_links.py
+++ get_links.py
@@ -8,29 +8,35 @@ Created on Wed Apr 12 09:03:55 2023
 import requests
 import json
 from bs4 import BeautifulSoup
-
-url = "https://en.wikipedia.org/w/api.php?action=parse&page=Elephant&format=json"
-response = requests.get(url)
-html = json.loads(response.content.decode('utf-8'))['parse']['text']['*']
-soup = BeautifulSoup(html, 'html.parser')
-links = soup.find_all('a', href=lambda href: href and href.startswith('/wiki/') and not href.endswith('.jpg') and not href.endswith('.png'))
-result = []
-for link in links:
-    href = link.get('href')
-    title = link.get('title')
-    text = link.text
-    # Sometimes a link is just a redirect to a different page
-    redirect_url = f"https://en.wikipedia.org{href}"
-    redirect_response = requests.get(redirect_url)
-    redirect_html = redirect_response.content.decode('utf-8')
+def links_on_page(animal_name="Elephant"):
+    url = "https://en.wikipedia.org/w/api.php?action=parse&page="+animal_name+"&format=json"
+    response = requests.get(url)
+    html = json.loads(response.content.decode('utf-8'))['parse']['text']['*']
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.find_all('a', href=lambda href: href and href.startswith('/wiki/') and not href.endswith('.jpg') and not href.endswith('.png'))
+    result = []
+    for link in links:
+        href = link.get('href')
+        title = link.get('title')
+        text = link.text
+        if False:
+            # Sometimes a link is just a redirect to a different page
+            redirect_url = f"https://en.wikipedia.org{href}"
+            redirect_response = requests.get(redirect_url)
+            redirect_html = redirect_response.content.decode('utf-8')
+            
+            if "#REDIRECT" in redirect_html:
+                redirect_soup = BeautifulSoup(redirect_html, 'html.parser')
+                new_title = redirect_soup.find('link', {'rel': 'canonical'}).get('title') #not all pages contain redirect div
+                if new_title is None:
+                    new_title = redirect_soup.find('title').text.replace(' - Wikipedia', '')
+                #print(f"Link: {href}\nRedirects to: {new_title}\nText: {text}\n")
+            #else:
+                #print(f"Link: {href}\nTitle: {title}\nText: {text}\n")
     
-    if "#REDIRECT" in redirect_html:
-        redirect_soup = BeautifulSoup(redirect_html, 'html.parser')
-        new_title = redirect_soup.find('link', {'rel': 'canonical'}).get('title') #not all pages contain redirect div
-        if new_title is None:
-            new_title = redirect_soup.find('title').text.replace(' - Wikipedia', '')
-        print(f"Link: {href}\nRedirects to: {new_title}\nText: {text}\n")
-    else:
-        print(f"Link: {href}\nTitle: {title}\nText: {text}\n")
+        result.append(href)
+    return result
 
-    result.append(href)
+if __name__ == "__main__":
+    lists = links_on_page(animal_name="Elephant")
+    
