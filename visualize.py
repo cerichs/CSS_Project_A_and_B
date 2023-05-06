df7a1631acd2547532002dc6ef27373cd8f08463
Ottohesten <otto@skytop.dk>
2023-05-06 17:15:16 +0200

    small changes to text analysis

--- visualize.py
+++ visualize.py
@@ -16,16 +16,16 @@ if __name__ == "__main__":
     with open('data/data_plain_long_reptile.pickle', 'rb') as handle:
         b = pickle.load(handle)
     
-    values = list(a.values())
+    values = list(b.values())
     plt.hist(values, bins=max(values), edgecolor='black')
     plt.xticks(range(1, max(values) + 1))
     plt.xlabel('Number of references')
     plt.ylabel('Frequency')
     plt.show()
     
-    edgelist = [None]*len(a)
-    for i,items in enumerate(a):
-        edgelist[i] = (items[0].replace("/wiki/", ""),items[1].replace("/wiki/", ""),int(a[items]))
+    edgelist = [None]*len(b)
+    for i,items in enumerate(b):
+        edgelist[i] = (items[0].replace("/wiki/", ""),items[1].replace("/wiki/", ""),int(b[items]))
     G = nx.DiGraph()
 
     #for entries in edgelist:
